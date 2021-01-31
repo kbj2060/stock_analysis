56199ace4dd4300a128f2021993f7f5106b72ffb
llewyn <kbj2060@naver.com>
2021-01-31 16:07:48 +0900

    clean up

--- script.py
+++
@@ -1,6 +0,0 @@
-import os
-
-x1 = sorted(os.listdir('./stock'))
-for x in x1:
-    fs = os.listdir('./stock/{0}/'.format(x))
-    print([os.rename('./stock/{0}/{1}'.format(x, f), './stock/{0}/{1}'.format(x, f[:-3])) for f in fs if str(f)[-2:] == 'h5'])
